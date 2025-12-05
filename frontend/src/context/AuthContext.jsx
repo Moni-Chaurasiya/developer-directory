e464c1459878256f34c5d79f239b854d04e0a9e2
Moni-Chaurasiya <monichaurasiya993@gmail.com>
2025-12-05 16:44:31 +0530

    feat: AuthContext logic added

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,88 @@
+import {createContext,
+        useContext,
+        useState,
+       // useEffect
+       } from 'react';
+import toast from 'react-hot-toast';
+import {useNavigate} from 'react-router-dom'
+
+const AuthContext=createContext();
+
+export const useAuth=()=>{
+    const context = useContext(AuthContext);
+    if(!context){
+        throw new Error('useAuth must be used within AuthProvider');
+
+    }
+    return context;
+}
+
+export const AuthProvider= ({children})=>{
+
+
+    const [user, setUser]= useState(()=>{
+        const storedUser=localStorage.getItem('user');
+        return storedUser ? JSON.parse(storedUser): null;
+    });
+    const [token, setToken]=useState(()=>localStorage.getItem('token'));
+    const navigate=useNavigate();
+
+//   const [user, setUser] = useState(null);
+//   const [token, setToken] = useState(null);
+
+//    const [loading,setLoading]=useState(true);
+
+    // useEffect(()=>{
+    //     const storedToken=localStorage.getItem('token');
+    //     const storedUser= localStorage.getItem('user');
+    //     if(storedToken && storedUser){
+    //         setToken(storedToken);
+    //         setUser(JSON.parse(storedUser))
+    //     }
+    //     setLoading(false);
+    // },[])
+
+    const login= (userData,authToken)=>{
+        setUser(userData);
+        setToken(authToken);
+        localStorage.setItem('token', authToken);
+        localStorage.setItem('user', JSON.stringify(userData))
+        toast.success('Login Successful');
+        navigate('/')
+    }
+
+    const signup=(userData,authToken)=>{
+    setUser(userData);
+    setToken(authToken);
+    localStorage.setItem('token', authToken);
+    localStorage.setItem('user', JSON.stringify(userData));
+    toast.success('Account created successfully!');
+    navigate('/');
+    }
+    
+    const logout =()=>{
+        setUser(null);
+        setToken(null);
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        toast.success('Logged out successfully');
+        navigate('/login')
+    }
+
+    const value={
+        user,
+        token,
+       // loading,
+        login,
+        signup,
+        logout,
+        isAuthenticated: !!token
+    }
+    return (
+
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
