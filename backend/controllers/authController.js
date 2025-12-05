24d5f8a010519b87dd61c12ef522fa97b5aafa88
Moni-Chaurasiya <monichaurasiya993@gmail.com>
2025-12-05 11:31:28 +0530

    feat: auth controller and middleware done

---
+++ backend/controllers/authController.js
@@ -0,0 +1,94 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+exports.signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.find({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists with this email",
+      });
+    }
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+        token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = User.find({ email });
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    const isPasswordValid = await user.comparePasssword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email password",
+      });
+    }
+
+    const token = generateToken(user._id);
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: {
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+        token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getMe= async(req,res,next)=>{
+    try {
+        // req.user is set by auth middleware so that repetition can be avoided
+        res.status(200).json({
+            success:true,
+            data:{
+                user:{
+                    id:req.user._id,
+                    name:req.user.name,
+                    email:req.use.email
+                }
+            }
+        })
+    } catch (error) {
+        next(error);
+    }
+}
